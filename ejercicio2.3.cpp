1ca5bbc547b65dcec15d9be36b4ace6f6ba6b6ef
fc-j <francisco.brokz@gmail.com>
2024-08-27 22:38:48 -0500

    ejercicios Capitulo 2

---
+++ ejercicio2.3.cpp
@@ -0,0 +1,37 @@
+/* APARTADO DE EJERCICIOS 2.3 */
+
+#include <iostream>
+using namespace std; 
+int main () {
+    /* ejercico (a) */
+    int  c=0, thiIsAVariable=0, q76354=0; 
+    /*----------------------------------*/
+    int val; 
+
+    /* ejercico (b) */
+    cout<<"ingrese un valor entero: ";cin>>val; 
+    /*----------------------------------*/
+    
+    if(val>15){
+        /* ejercico (d) */
+        cout<<"el nuevo valor es: "<<val-5<<endl; 
+    }
+    else{
+        /* ejercico (c) */
+        cout<<"el valor mas 10 es: "<<val+10<<endl; 
+    }
+        /* ejercicio (e) */
+    cout<<"This i a c++ program"<<endl; 
+
+        /* ejercicio (f) */
+    cout<<"This i a c++ \nprogram"<<endl; 
+
+        /* ejercicio (g) */
+    cout<<"This \nis \na \nc++ \nprogram"<<endl; 
+
+        /* ejercicio (h) */
+    cout<<"This \tis \ta \tc++ \tprogram"<<endl; 
+
+     
+
+}
---
+++ ejercicio2.4.cpp
@@ -0,0 +1,30 @@
+/* APARTADO DE EJERCICIOS 2.4 y 2.5*/
+
+#include <iostream>
+using namespace std; 
+int main () {
+
+    /* ejercicio (b) */
+    int x; 
+    int y; 
+    int z; 
+    int result; 
+    
+    /* ejercicio (c) */
+    cout<<"digite 3 numeros"<<endl; 
+
+    /* ejercicio (d) */
+    cout<<"x: "; cin>>x; 
+    cout<<"y: "; cin>>y; 
+    cout<<"z: "; cin>>z; 
+
+    /* ejercicio (e) */
+    result=x*y*z; 
+
+    /* ejercicio (f) */
+    cout<<"el valor es: "<<result; 
+    
+    /* ejercicio (g) */
+    return 0; 
+
+}
